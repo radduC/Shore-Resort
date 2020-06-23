463bc4d99d37b59e286bef6c2bd46dcc8280f191
radu costache <sir.raddu@gmail.com>
2020-06-23 21:34:03 +0300

    Updates

---
+++ precache-manifest.2f9f274500b2498dd9ed183352e1c632.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0bba27308f72cf40f096fbbf0665826",
+    "url": "/Shore-Resort/index.html"
+  },
+  {
+    "revision": "be8675331c2f9bed24de",
+    "url": "/Shore-Resort/static/css/2.e62b1ac3.chunk.css"
+  },
+  {
+    "revision": "5ea856b3a228410e8ea8",
+    "url": "/Shore-Resort/static/css/main.b964b93c.chunk.css"
+  },
+  {
+    "revision": "be8675331c2f9bed24de",
+    "url": "/Shore-Resort/static/js/2.0e3bb38a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Shore-Resort/static/js/2.0e3bb38a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ea856b3a228410e8ea8",
+    "url": "/Shore-Resort/static/js/main.e51860b1.chunk.js"
+  },
+  {
+    "revision": "04dfed5ef8a322a83bcd",
+    "url": "/Shore-Resort/static/js/runtime-main.846b96c3.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Shore-Resort/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Shore-Resort/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Shore-Resort/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Shore-Resort/static/media/slick.f97e3bbf.svg"
+  }
+]);
